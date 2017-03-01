6072d9b99d40f1855d38b7b33e21f71b2d006ceb
Nem Sothea <NemSothea@NemSothea.local>
2017-03-01 17:40:46 +0700

    nearly finish the code

--- FirebaseLogin/ViewController.swift
+++ FirebaseLogin/ViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBSDKLoginButtonDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 280, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Do you really wanna logout?")
+    }
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        print("Successfully loged in with facebook account..:)")
     }
 
 
